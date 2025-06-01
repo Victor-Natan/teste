e86e9976f97d67e25fae1db07fad57db879511b3
Victor-Natan <victornatan280@gmail.com>
2025-06-01 01:27:00 -0300

    deixando todos os links ativos e retirando as imagens do produto.html

--- main.js
+++ main.js
@@ -3,30 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const produto = urlParams.get("produto");
   
     const produtos = {
-      manjericao: {
-        produto: `Manjericão`,
-        titulo: `Benefícios do Manjericão`,
-        beneficios: [
-            `1. Antioxidantes: Protege as células e promove uma pele saudável.`,
-            `2. Apoio imunológico: Fortalece o sistema imunológico e ajuda a manter as doenças à distância.`,
-            `3. Digestão leve: Alivia o sistema digestivo, tornando as refeições mais agradáveis.`,
-            `4. Redução de estresse: O aroma relaxante ajuda a diminuir a ansiedade.`,
-            `5. Versatilidade na cozinha: Use em saladas, molhos ou smoothies para dar um toque especial.`
-        ],
-        imagem: "img/manjericao_img.jpg"
-      },
-      lavanda: {
-        produto: `Lavanda`,
-        titulo: `A magia da Lavanda`,
-        beneficios: [
-          `🌸 Acalma e alivia o estresse.`,
-          `😴 Ajuda no sono.`,
-          `🔥 Propriedades anti-inflamatórias.`,
-          `🌿 Aroma relaxante.`,
-        ],
-        imagem: "img/lavanda_img.jpg"
-      },
-      
       canela: {
         produto: `Canela`,
         titulo: `A magia da Canela no dia a dia<br><br>`,
@@ -44,17 +20,6 @@ document.addEventListener("DOMContentLoaded", () => {
           `Falta de Oportunidades`,
           `Estagnação<br><br>`
         ],
-        titulo_sessão_produto: `PRODUTOS`,
-        imagem: [
-            `img/Captura de tela 2025-04-18 060658.png`, 
-            `img/Captura de tela 2025-04-18 062248.png`,
-            `img/Captura de tela 2025-04-18 063114.png`,
-            `img/Captura de tela 2025-04-18 063325.jpg`,
-            `img/Captura de tela 2025-04-18 172545.png`,
-            `img/Captura de tela 2025-04-18 172729.png`,
-            `img/Captura de tela 2025-04-18 172848.jpg`,
-            `img/Captura de tela 2025-04-18 173027.png`,
-        ],
         texto_descritivo: [
             `<br><br>FORTUNA e PODER`,
             `<br><br>Incenso da bruxa`,
@@ -65,30 +30,272 @@ document.addEventListener("DOMContentLoaded", () => {
             `<br><br>Spray Energético`,
             `<br><br>Chá Bruxa`,
         ]
-      }
+      },
+
+
+      lavanda: {
+        produto: `Lavanda`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+      
+      incenso: {
+        produto: `Incenso`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+      
+      papelaria: {
+        produto: `Papelaria`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+
+      manjericao: {
+        produto: `Manjericao`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+
+      vela: {
+        produto: `Vela`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+
+      eucaplito: {
+        produto: `Eucaplito`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+
+      palo: {
+        produto: `Palo`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
+      
+      anis: {
+        produto: `Anis`,
+        titulo: `A magia da Canela no dia a dia<br><br>`,
+        beneficios: [
+          `<strong>Atrai</strong>`,
+          `Sucesso Financeiro`,
+          `Paixão nos Relacionamentos`,
+          `Sucesso em Empreendimentos`,
+          `Prosperidade`,
+          `Boas Energias<br><br>`,
+          `<strong>Afasta</strong>`,
+          `Falta de Dinheiro`,
+          `Dificuldade nos Negócios`,
+          `Olhado-Mau`,
+          `Falta de Oportunidades`,
+          `Estagnação<br><br>`
+        ],
+        titulo_sessão_produto: `PRODUTOS`,
+        texto_descritivo: [
+            `<br><br>FORTUNA e PODER`,
+            `<br><br>Incenso da bruxa`,
+            `<br><br>Wax Bars`,
+            `<br><br>Momento Aconchego`,
+            `<br><br>Banho e Escalda Pés`,
+            `<br><br>Chá Amor`,
+            `<br><br>Spray Energético`,
+            `<br><br>Chá Bruxa`,
+        ]
+      },
     };
   
     const container = document.getElementById("conteudo-produto");
   
     if (produto && produtos[produto]) {
-        const dados = produtos[produto];
-        container.innerHTML = `
-          <h1 class="titulo">${dados.produto}</h1>
-          <section class="sobre">  
-            <h1 class="sobre__titulo">${dados.titulo}</h1>
-            ${dados.beneficios.map(texto => `<p class="sobre__texto">${texto}</p>`).join("")}
-            <div class="imagens-container">
-              <h1 class="titulo_sessão_produto">${dados.titulo_sessão_produto}</h1>
-              ${dados.imagem.map((imagem, index) => `
-                <div class="imagem-com-texto">
-                  ${dados.texto_descritivo && dados.texto_descritivo[index] ? 
-                    `<div class="texto-sobre-imagem">${dados.texto_descritivo[index]}</div>` : ''}
-                  <img src="${imagem}" alt="${dados.produto}" class="produto__imagem">
-                </div>
-              `).join("")}
-            </div>
-          </section>
-        `;
+  const dados = produtos[produto];
+  container.innerHTML = `
+    <h1 class="titulo">${dados.produto}</h1>
+    <section class="sobre">  
+      <h1 class="sobre__titulo">${dados.titulo}</h1>
+      ${dados.beneficios.map(texto => `<p class="sobre__texto">${texto}</p>`).join("")}
+      <div class="imagens-container">
+        <h1 class="titulo_sessão_produto">${dados.titulo_sessão_produto}</h1>
+        ${dados.texto_descritivo && dados.texto_descritivo.length
+          ? dados.texto_descritivo.map(texto => `
+              <div class="imagem-com-texto">
+                <div class="texto-sobre-imagem">${texto}</div>
+              </div>
+            `).join("")
+          : ""}
+      </div>
+    </section>
+  `;
+
       } else {
       container.innerHTML = "<p>Produto não encontrado.</p>";
     }
